15a75634fc71e41dc4f013fc80a757fde834e082
Avri Chen-Roth <the-mentor@users.noreply.github.com>
2026-03-16 19:41:54 +0200

    Initial commit

---
+++ PSmac/internal/scripts/intialize.ps1
@@ -0,0 +1,6 @@
+﻿# Commands run on module import go here
+# E.g. Argument Completers could be placed here
+
+if (!$IsMacOS) {
+	Write-Warning "This module is only supported on macOS. Importing it on other platforms may lead to unexpected behavior or errors."
+}
---
+++ build/vsts-prerequisites.ps1
@@ -0,0 +1,34 @@
+﻿<#
+Uses PowerShellGet to install all modules required to run the pipeline.
+#>
+[CmdletBinding()]
+param (
+	[string]
+	$Repository = 'PSGallery'
+)
+
+$modules = @(
+	'Pester' # Testing Framework
+	'PSScriptAnalyzer' # Best Practices Analyzer used during tests
+	'Microsoft.PowerShell.PlatyPS' # Generate docs from help
+	'PSModuleDevelopment' # Potentially used in Tests or Publish
+)
+
+# Automatically add missing dependencies
+$data = Import-PowerShellDataFile -Path "$PSScriptRoot\..\PSmac\PSmac.psd1"
+foreach ($dependency in $data.RequiredModules) {
+	if ($dependency -is [string]) {
+		if ($modules -contains $dependency) { continue }
+		$modules += $dependency
+	}
+	else {
+		if ($modules -contains $dependency.ModuleName) { continue }
+		$modules += $dependency.ModuleName
+	}
+}
+
+foreach ($module in $modules) {
+	Write-Host "Installing $module" -ForegroundColor Cyan
+	Install-Module $module -Force -SkipPublisherCheck -Repository $Repository
+	Import-Module $module -Force -PassThru
+}
