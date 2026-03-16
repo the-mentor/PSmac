04316d3200c6d982ba84e7d75b5c789df3de284d
Avri Chen-Roth <the-mentor@users.noreply.github.com>
2026-03-16 22:02:03 +0200

    Add Start-MacOSApp Cmdlet

---
+++ PSmac/functions/Start-MacOSApp.ps1
@@ -0,0 +1,20 @@
+﻿function Start-MacOSApp { 
+    param(
+        [Parameter(Mandatory, Position = 0)]
+        [ValidateScript(
+            { $_ -in (Get-ChildItem -Path @("/Applications", "~/Applications") -Filter *.app | Select-Object   @{l = "Name"; e = { $_.name.Replace('.app', '') } }).Name },
+            ErrorMessage = 'Please specify the name of a subdirectory in the current directory.'
+        )]
+        [ArgumentCompleter(
+            {
+                param($cmd, $param, $wordToComplete)
+                # This is the duplicated part of the code in the [ValidateScipt] attribute.
+                [array] $validValues = (Get-ChildItem -Path @("/Applications", "~/Applications") -Filter *.app | Select-Object   @{l = "Name"; e = { $_.name.Replace('.app', '') } }).Name
+                $validValues -like "$wordToComplete*"
+            }
+        )]
+        [String]$AppName
+    )
+        
+    open -a "$AppName"
+}
