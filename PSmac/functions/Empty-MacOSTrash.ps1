7f7597bee53287d668e91fbefed94fa006cde9d6
Avri Chen-Roth <the-mentor@users.noreply.github.com>
2026-03-16 19:41:54 +0200

    Initial commit

---
+++ PSmac/functions/Empty-MacOSTrash.ps1
@@ -0,0 +1,4 @@
+﻿function Empty-MacOSTrash {
+	# Empty the Trash on macOS per https://superuser.com/questions/1877663/how-can-i-empty-the-trash-from-the-macos-terminal
+	osascript -e 'try' -e 'tell application "Finder" to empty' -e 'end try'
+}
---
+++ PSmac/internal/scripts/intialize.ps1
@@ -0,0 +1,16 @@
+﻿# Commands run on module import go here
+# E.g. Argument Completers could be placed here
+
+if (!$IsMacOS) {
+	Write-Warning "This module is only supported on macOS. Importing it on other platforms may lead to unexpected behavior or errors."
+}
+else {
+	Write-Verbose "Running on macOS, proceeding with module initialization."
+
+	# Init Brew if it exists.
+	if (Get-Item -Path /opt/homebrew/bin/brew -ErrorAction SilentlyContinue) {
+		/opt/homebrew/bin/brew shellenv | Invoke-Expression
+	}
+}
+
+
